4992aa1a5ba3b13464ed850870017aab206f9f96
MagicianThirteen <MagicianThirteen@gmail.com>
2022-03-09 22:51:22 +0800

    添加爬楼梯非递归解法

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ namespace LeetCode
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            L_70 l_70 = new L_70();
-            Console.WriteLine($"当前有3个楼梯时，总共有{l_70.ClimbStairs(3)}种解法");
+            L_70 l_70 = new L_70(); 
+            Console.WriteLine($"当前有6个楼梯时，总共有{l_70.ClimbStairs(6)}种解法");
         }
         private  Dictionary<int, int> saveResult = new Dictionary<int, int>();
         public  int ClimbStairs(int n)
         {
             if (n == 1) return 1;
-            if(n == 2) return 2;
+            if (n == 2) return 2;
             if (saveResult.ContainsKey(n))
             {
                 return saveResult[n];
@@ -28,7 +28,18 @@ namespace LeetCode
                 saveResult.Add(n, result);
                 return result;
             }
-
+            /*if (n == 1) return 1;
+            if(n == 2) return 2;
+            int pre = 2;
+            int prePre = 1;
+            int result=0;
+            for(int i = 3; i <= n; i++)
+            {
+                result = pre + prePre;
+                prePre = pre;
+                pre = result;
+            }
+            return result;*/
         }
     }
 }
