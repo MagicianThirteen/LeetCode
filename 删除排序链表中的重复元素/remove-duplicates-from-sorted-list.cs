0a46e19ed3d0d2427e70b5faef6cdfdbb9f21e20
MagicianThirteen <MagicianThirteen@gmail.com>
2022-10-20 21:08:51 +0800

    删除有序链表中的重复元素

---
+++ 删除排序链表中的重复元素/remove-duplicates-from-sorted-list.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.删除排序链表中的重复元素
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    internal class removeduplicatesfromsortedlist
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            //一般做法
+            if (head == null) return null;
+            ListNode current = head;
+            while (current.next != null)
+            {
+                if (current.next.val == current.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+            return head;
+
+            //递归解法
+            /*if (head == null) return head;
+            head.next = DeleteDuplicates(head);
+            return head.val == head.next.val ? head.next : head;*/
+        }
+    }
+}
---
+++ 合并两个有序链表/MergeTwoLists.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.合并两个有序链表
+{
+ 
+
+  public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+ 
+    internal class MergeTwoLists
+    {
+        public ListNode Sort(ListNode list1,ListNode list2)
+        {
+            //定义个临时的存储结果的list
+            ListNode result = new ListNode();
+            ListNode tmp=new ListNode();
+            result = tmp;
+            //边界条件,l1和l2一个有一个没有
+            if (list1 == null)
+            {
+                return list2;
+            }
+            if (list2 == null)
+            {
+                return list1;
+            }
+            //当双方链表都有时
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    tmp.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    tmp.next = list2;
+                    list2 = list2.next;
+                }
+                tmp = tmp.next;
+            }
+            if (list1.next == null)
+            {
+                tmp.next = list2;
+            }
+            if (list2.next == null)
+            {
+                tmp.next = list1;
+            }
+            return result;
+        }
+    }
+}
